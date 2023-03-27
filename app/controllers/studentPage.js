0de1bc83dddc3d0e2d79e3ac8971cfea5e258940
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-03-27 15:22:09 -0300

    Página de erro para caso não seja passado uma matricula como parametro na url de studentPage

--- app/controllers/studentPage.js
+++ app/controllers/studentPage.js
@@ -1,14 +1,18 @@
 module.exports.homePage = (app, req, res) => {
+
     const params = req.query;
-    const matricula = params.matricula;
+    if(params.length == undefined){
+        res.send("No user found")
+    }else{
+        const matricula = params.matricula;
 
-    const connection = require('../../config/dbConnection');
-    const model = new app.app.models.AlunosDAO(connection);
+        const connection = require('../../config/dbConnection');
+        const model = new app.app.models.AlunosDAO(connection);
 
-    model.obterAluno(matricula, (error, result) => {
-        res.render('admin/studentPage/studentPage', {aluno: result});
-    });
-    
+        model.obterAluno(matricula, (error, result) => {
+            res.render('admin/studentPage/studentPage', {aluno: result});
+        });
+    }
 
     
 }
