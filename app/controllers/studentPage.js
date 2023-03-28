1c50bed72bc551c429ccfee908a1e4faedd9d695
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-03-28 18:57:06 -0300

    Inicio de validação por chave

--- app/controllers/studentPage.js
+++ app/controllers/studentPage.js
@@ -1,19 +1,21 @@
 module.exports.homePage = (app, req, res) => {
-
+    const key = require('../../key');
     const params = req.query;
-    
-    if(params.matricula == undefined){
-        res.send("No user found");
-    }else{
-        const matricula = params.matricula;
+    if(req.query.key == key){
+        if(params.matricula == undefined){
+            res.send("No user found");
+        }else{
+            const matricula = params.matricula;
 
-        const connection = require('../../config/dbConnection');
-        const model = new app.app.models.AlunosDAO(connection);
+            const connection = require('../../config/dbConnection');
+            const model = new app.app.models.AlunosDAO(connection);
 
-        model.obterAluno(matricula, (error, result) => {
-            res.render('admin/studentPage/studentPage', {aluno: result});
-        });
+            model.obterAluno(matricula, (error, result) => {
+                res.render('admin/studentPage/studentPage', {aluno: result});
+            });
+        }
+    } else{
+        res.send('No validation');
     }
-
     
 }
---
+++ key.js
@@ -0,0 +1,3 @@
+const key = "98df7s9f0rjs0a9f8sdfdkfsd-9u0dfj809s8fdd";
+
+module.exports = key;
