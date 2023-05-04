f3477746d9c16cf108cc3677ff6ca0f5c8d4f004
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-05-04 17:24:52 -0300

    Alterando caracteres de chars do arquivo key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$%*";
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 let key = "";
 for(var i = 0; i < 22; i++){
     key = key + chars.charAt(Math.floor(Math.random() * chars.length));
