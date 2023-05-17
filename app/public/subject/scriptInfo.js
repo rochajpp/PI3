4b50668a1b9de019c29b0831554d0394c71d3383
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-05-17 19:35:01 -0300

    Sistema de pop feito

---
+++ app/public/subject/scriptInfo.js
@@ -0,0 +1,7 @@
+function addPop(){
+    const main = document.querySelector(".main");
+    const body = document.querySelector("body");
+    main.classList.toggle("active");
+    body.classList.toggle("activeBody");
+}
+
